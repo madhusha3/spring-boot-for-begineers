e20862ba2369417167e6b30bd6ed2f6b7b2e107f
madhusha3 <madhusudan2319@gmail.com>
2023-03-25 01:57:54 +0530

    initializing spring boot project

---
+++ src/test/java/com/madhu/springdemo/SpringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madhu.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
